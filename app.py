31a51401a40c9dd7932e7304702419c32fd9969d
Oliver Wirth <oliver.wirth@bechtle.com>
2015-06-19 15:49:10 +0200

    Searching for bug in inconsequent session returns

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def admin() :
 
 @app.errorhandler(403)
 @app.errorhandler(404)
-#@app.errorhandler(500)
+@app.errorhandler(500)
 def throw(error) :
 	return HomeController.error(error.code)
 
--- utils/Session.py
+++ utils/Session.py
@@ -3,6 +3,7 @@
 from flask import session
 
 from utils.Compression import decode, encode
+from utils import Log
 
 
 class UserSession() :
@@ -12,11 +13,13 @@ class UserSession() :
 		self.key = key
 
 	def get(self) :
+		Log.debug("Session::get", user = self.session.get(self.key))
 		user = self.session.get(self.key)
 		if user :
 			return decode(user)
 
 	def set(self, user) :
+		Log.debug("Session::set" user = user)
 		if user :
 			self.session[self.key] = encode(user)
 
@@ -24,6 +27,7 @@ class UserSession() :
 		self.session.pop(self.key, None)
 
 	def exists(self) :
+		Log.debug("Session::exists", exists = self.session.has_key(self.key))
 		return self.session.has_key(self.key)
 
 	def remember(self, val) :
