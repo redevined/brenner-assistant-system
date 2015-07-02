327d90f0302259c6bda9cafbb67b01782993c013
Oliver Wirth <oliver.wirth@bechtle.com>
2015-07-02 16:56:18 +0200

    Trying to fix

--- controllers/CourseController.py
+++ controllers/CourseController.py
@@ -12,8 +12,6 @@ def view(pdf = "") :
 	user = User.session.get()
 	courses = Course.getAll(user.username)
 	months = { u"{0} {1}".format(Months.get[int(course.date.split(".")[1]) - 1], course.date.split(".")[2]) for course in courses }
-	if pdf :
-		return download({ "pdf" : pdf })
 	return render_template("courses.html", user = user, courses = courses, months = months, pdf = pdf)
 
 def add(form) :
