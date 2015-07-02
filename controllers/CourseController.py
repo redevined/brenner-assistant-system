275bb9525acac73c2106bda79b7dce6fc566bb3f
Oliver Wirth <oliver.wirth@bechtle.com>
2015-07-02 15:38:40 +0200

    More debugging

--- controllers/CourseController.py
+++ controllers/CourseController.py
@@ -12,7 +12,7 @@ def view(pdf = "") :
 	user = User.session.get()
 	courses = Course.getAll(user.username)
 	months = { u"{0} {1}".format(Months.get[int(course.date.split(".")[1]) - 1], course.date.split(".")[2]) for course in courses }
-	return render_template("courses.html", user = user, courses = courses, months = months, pdf = pdf)
+	return "WTF" #render_template("courses.html", user = user, courses = courses, months = months, pdf = pdf)
 
 def add(form) :
 	if User.session.exists() :
